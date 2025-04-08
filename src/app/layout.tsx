c544d11e4413f38248282b0e8e2a3161ea8b1b58
Temp User <temp@example.com>
2025-04-08 23:51:13 +0400

    تحديث عنوان الموقع

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Street Fighter - FOXLIVE',
+  title: "FOX Street Fighter Tournament",
   description: 'تحديات ستريت فايتر على FOXLIVE',
 }
 
